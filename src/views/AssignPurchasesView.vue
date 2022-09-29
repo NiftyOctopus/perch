55d592f71b74f24271dcdc35f8461e435862bb7c
Bryan Chambers <bryches@gmail.com>
2022-09-29 11:16:12 -0400

    Remove assigned purchase from state

--- src/views/AssignPurchasesView.vue
+++ src/views/AssignPurchasesView.vue
@@ -3,21 +3,22 @@
     import PurchaseGroup from '../components/PurchaseGroup.vue'
     import Select        from '../components/Select.vue'
 
-    const cats = ['Restaurants', 'Streaming', 'Trains']
+    const cats = ['Gaming', 'Groceries', 'Restaurants', 'Rides', 'Streaming', 'Trains']
     const category = ref()
 
     function selectCategory(cat) {
         category.value = cat
     }
 
-    const unassigned = ref([])
+    const purchases = ref([])
     onMounted(() => getPurchases())
 
     async function getPurchases() {
         const dev  = 'http://127.0.0.1:5000'
         const prod = 'http://192.168.1.182:81'
-        const res  = await fetch(prod + '/purchases/unassigned')
-        unassigned.value = await res.json()
+
+        const res = await fetch(prod + '/purchases/unassigned')
+        purchases.value = await res.json()
     }
 
     async function assignPurchase(purchase) {
@@ -34,7 +35,27 @@
         }
 
         const res = await fetch(prod + '/purchases/' + purchase.id + '/assign', options)
-        console.log(await res.json())
+        
+        const {success} = await res.json()
+        if(success) removePurchase(purchase)
+    }
+
+    function removePurchase(p) {
+        const updated = purchases.value.map((group) => {
+            if(group.date === p.date) return removePurchaseFrom(group, p)
+            return {...group}
+        })
+
+        purchases.value = updated
+    }
+
+    function removePurchaseFrom(group, purchase) {
+        const updated = group.purchases.filter((p) => p.id !== purchase.id)
+        
+        return {
+            date: group.date,
+            purchases: [...updated]
+        }
     }
 </script>
 
@@ -45,7 +66,7 @@
 
     <div id='results'>
         <PurchaseGroup
-            v-for='group in unassigned'
+            v-for='group in purchases'
             :key='group.date'
             :group='group'
             @select='assignPurchase'
