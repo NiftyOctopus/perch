e2aabd9b19eef0f17f4fd63b7c5b30690e7d015a
Bryan <bgchambers@pm.me>
2022-11-10 18:44:01 -0500

    Added time selector and group by owner

--- src/views/SumByCatView.vue
+++ src/views/SumByCatView.vue
@@ -1,19 +1,22 @@
 <script setup>
     import { ref, onMounted, watch } from 'vue'
-    //import PurchaseGroup from '../components/PurchaseGroup.vue'
+    import Select from '../components/Select.vue'
 
-    const days = ref(7)
-    watch(days, getSums)
+    const times = ['1w', '2w', '1m', '3m']
+    const time  = ref('1w')
+
+    function selectTime(t) { time.value = t }
+    watch(time, getSums)
 
     const sums = ref([])
     onMounted(() => getSums())
 
     async function getSums() {
-        if(!days.value) return
+        if(!time.value) return
         const dev  = 'http://127.0.0.1:5000'
         const prod = 'http://192.168.1.182:81'
 
-        const res  = await fetch(prod + '/sum?by-cat=true&days=' + days.value)
+        const res  = await fetch(prod + '/sum?by-cat=true&time=' + time.value)
         sums.value = await res.json()
     }
 
@@ -21,29 +24,38 @@
     const sorted = ref([])
 
     watch(sums, newSums => {
-        sorted.value = newSums.sort((a, b) => {
+        sorted.value = newSums.map(group => sortSums(group))
+    })
+
+    function sortSums(group) {
+        const sorted = group.data.sort((a, b) => {
             if(a > b) return  1
             if(b > a) return -1
             return 0
         })
 
-        total.value = newSums.reduce((total, item) => {
+        const total = group.data.reduce((total, item) => {
             return total + item.sum
         }, 0)
-    })
+
+        return { owner: group.owner, total, data: sorted }
+    }
 </script>
 
 
 <template>
     <div class='view'>
         <h3>Sum by Category</h3>
-        <input v-model='days' type='number' min=0 />
-        <p>Last {{ days }} days</p>
-        <p>{{ total.toFixed(2) }} Total</p>
+        <Select :options='times' @select='selectTime' :selected='time' />
 
         <div v-if='sorted && sorted.length > 0'>
-            <div v-for='item in sorted'>
-                {{ item.sum }} {{ item.category ? item.category : 'None' }}
+            <div v-for='group in sorted' style='border: 1px dashed lightgray; margin-bottom: 1em;'>
+                <div><strong>{{ group.owner }}</strong></div>
+                <div><strong>{{ group.total.toFixed(2) }}</strong></div>
+
+                <div v-for='item in group.data'>
+                    {{ item.sum }} {{ item.category ? item.category : 'None' }}
+                </div>
             </div>
         </div>
     </div>
